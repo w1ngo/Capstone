438eccce3632668fd02d3bda12669f34dacd58a1
Holden Hill <holden.onestarflag@gmail.com>
2023-02-13 16:34:47 -0600

    Consolidated edge detection & measurement + improved speed
    This commit adds a lot of documentation to the python version
    of the edge detection stage, as well as consolidating some
    of the functionality. It was made more modular initially to
    ease development and debugging, and consolidating afforded
    the ability to increase speed of execution.

--- dev/edge_detect/python/detection_functions.py
+++ dev/edge_detect/python/detection_functions.py
@@ -15,7 +15,9 @@ Returns a string filename pointing to the image to process
 def read_filename():
     args = argparse.ArgumentParser()
     args.add_argument( "--image", dest="image", action='store', type=str, required=False, help="path to input image" )
-    args = vars(args.parse_args())
+    
+    args     = vars(args.parse_args())
+    filename =  ""
 
     if args["image"]:
         filename = args["image"]
@@ -33,37 +35,16 @@ This function uses its string parameter to open and read a file storing processi
 It needs only the valid string filename, and returns a 2D list, with each elem being
     a set of parameters for processing
 '''
-def compile_param_list( filename ):
+def compile_param_list( filename="param_refine_all3.txt" ):
     params = []
 
-    with open("param_refine.txt", "r") as file:
+    with open(filename, "r") as file:
         for line in file:
             tmp = line.split()
-            params.append( [True, True, int(tmp[0]), int(tmp[1])] )
+            params.append( [int(tmp[0]), int(tmp[1])] )
 
     return params
     #ENDOF: compile_param_list()
-
-
-'''
-This function preps an image for processing using an image object, as well as two
-    state variables. The gauss param directs whether or not to use a Gaussian blur
-    (should likely always be true), and the median param directs whether or not to
-    use a median filter (for our potato application, this is vital).
-
-It returns an image object that has been properly filtered and can be passed for edge
-    detection
-'''
-def prep_img( img, gauss, median ):
-    if gauss:
-        img = cv2.GaussianBlur( img, [0, 0], 3, 0, cv2.BORDER_WRAP )
-
-    if median:
-        for i in range(10):
-            img = cv2.medianBlur( img, 7 )
-
-    return img
-    #ENDOF: prep_img()
 
 
 '''
@@ -75,106 +56,75 @@ This function combines many other functions in this file. It accepts a filename
     will make it shades of grey), and returned.
 
 It accepts the following parameters:
-    gauss    - boolean - Whether or not to prep with a gaussian filter
-    median   - boolean - Whether or not to prep with a median filter
     lthresh  - int     - The low hysterisis threshold to use for Canny
     hthresh  - int     - The high hysterisis threshold to use for Canny
     L2       - boolean - L2 gradient for Canny...leave False
     filename - string  - path to image filename
 '''
-def edge_detect( gauss, median, lthresh, hthresh, L2, filename ):
-    img = prep_img( cv2.imread( filename, cv2.IMREAD_GRAYSCALE ), gauss, median )
-    img = cv2.Canny( img, hthresh, lthresh, 3, L2gradient=L2 )
-    
-    # an additional GaussianBlur joins together edge separations
-    img = cv2.GaussianBlur(img, [3, 5], (1.0/3.0), 0, cv2.BORDER_WRAP)
-    
-    # applying a threshold puts the picture back into a binary pixel domain
-    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY)
-    return img
-    #ENDOF: edge_detect()
 
 
 '''
-Function used to detect the orientation & measurements for an edge-detected
-    image.
-
-It accepts one edge-detected image object as a parameter.
-
-It returns a 2D list, with each element being a 3-elem list
-    of information on each contour detected.
-        Elem one is a pair of coordinates for the centerpoint of the bounding rect.
-        Elem two is a pair of measurements for height & width of the bounding rect.
-        Elem three is the angle to which the bounding rect is askew.
-'''
-def orientation( img, debug_print=False ):        
-    data = []
-    cnts = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    ret = []
-    
-    for cnt in cnts:
-        if cv2.contourArea(cnt) < 10000:
-            continue
-           
-        rect = cv2.minAreaRect(cnt)
-
-        if debug_print:
-            box = np.int0(cv2.boxPoints(rect))
-            cv2.drawContours(img, [box], 0, (255,255,255), 3)
-            cv2.namedWindow("Box drawn", cv2.WINDOW_NORMAL)
-            cv2.imshow( "Box drawn", img )
-            cv2.waitKey(0)
-            
-        ret.append( rect )
-
-    return ret
-    #ENDOF: orientation()
-
-
-'''
-This function uses the orientation function with a set of processing parameters.
-    Rather than taking one processed image in and running orientation on it,
-    it accepts a list of parameters (such as the result of compile_param_list)
-    along with an image filename. It then averages the results of measuring
-    the image edges produced as output from each set of parameters, and returns
-    both the average height and average width
+This function implements edge detection and dimension measurement with a set of 
+    processing parameters. Rather than taking one processed image in and running
+    operations on it, it accepts a list of parameters (such as the result of 
+    compile_param_list) along with an image filename. It then performs the image
+    prep, the processing based on the passed-in parameters, and then measures 
+    those outputs. It finally averages those results of measuring and returns.
 
 Requires a string image filename parameter
 Requires a 2D list of parameters with each element being a list formatted:
-    <gauss>, <median>, <low thresh>, <high thresh>
+    <low thresh>, <high thresh>
 '''
 def find_measurements( img_filename, params ):
+    # values to return
     height  = 0
     width   = 0
-    
-    GAUSS   = 0
-    MEDIAN  = 1
-    LTHRESH = 2
-    HTHRESH = 3
+    tot     = 0
 
+    # index constants
+    LTHRESH    = 0
+    HTHRESH    = 1
     HEIGHT_IND = 0
     WIDTH_IND  = 1
 
-    for line in params:
-        img = edge_detect( line[GAUSS], line[MEDIAN], line[LTHRESH], line[HTHRESH], False, img_filename )
-        dat = orientation( img )
+    # read image & filter for prep
+        # as preprocessing is always the same, only do this once
+        # repeating this in the loop adds a TON of exec time
+    img = cv2.GaussianBlur( cv2.imread( img_filename, cv2.IMREAD_GRAYSCALE ), [0, 0], 3, 0, cv2.BORDER_WRAP )
+    for i in range(10):
+        img = cv2.medianBlur( img, 7 )
 
+    # process image foreach set of parameters
+    for line in params: 
+        img_edges = cv2.Canny( img, line[HTHRESH], line[LTHRESH], 3, L2gradient=False )
+        
+        # light blur & re-binarize image to join up broken edge pixels
+        img    = cv2.GaussianBlur(img, [3, 5], (1.0/3.0), 0, cv2.BORDER_WRAP)
+        _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY)
+        
+        # use inbuilt contour-locating fxn on input image object
+        cnts = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        dat  = [ cv2.minAreaRect(elem) for elem in cnts if cv2.contourArea(elem) > 100]
+        
+
+        # if did not receive clear information
         if len( dat ) == 0:
             continue
 
+        # TODO: implement box combination? or leave as a skip
         if len( dat ) > 1:
-            print( "Multiple bounding boxes located, handle separately..." )
-            return 0, 0
+            continue
 
-        print( dat )
-        dat = dat[0]
-        print( dat )
+        # retrieve bounding rect data from the list returned by orientation() and store
+        dat     = dat[0]
         height += dat[1][HEIGHT_IND]
         width  += dat[1][WIDTH_IND]
+        tot    += 1
 
-    height /= len(params)
-    width  /= len(params)
+    # compute averages based on number of used param combinations
+    height /= tot
+    width  /= tot
 
     return height, width
     #ENDOF: find_measurements
--- dev/edge_detect/python/fxn_tester.py
+++ dev/edge_detect/python/fxn_tester.py
@@ -16,18 +16,18 @@ if __name__ == "__main__":
         filename2 = r".\images\isolated_potato2.jpg"
         filename3 = r".\images\isolated_potato3.png"
 
-    if False:
-        _, data = refine.refine( [filename1] )
+    if input("Enter y to refine parameter options, or anything else to test edge detection as-is: ") == "y":
+        _, data = refine.multi_refine( [filename1, filename2, filename3] )
 
     else:
-        params = funcs.compile_param_list("param_refine.txt")
+        params = funcs.compile_param_list()
+        # print(f"params compiled...{len(params)} param sets identified\n\n")
 
         height, width = funcs.find_measurements(filename1, params)
         print( f"Height: {height}, Width: {width}" )
-
         height, width = funcs.find_measurements(filename2, params)
         print( f"Height: {height}, Width: {width}" )
-
         height, width = funcs.find_measurements(filename2, params)
         print( f"Height: {height}, Width: {width}" )
-
+        
+        cv2.waitKey(0)
