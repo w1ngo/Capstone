e58dea9619db7d0a02e63d23c51c878e6cbfc796
w1ngo <65159308+w1ngo@users.noreply.github.com>
2023-03-01 10:34:19 -0600

    Update file_io.py

--- dev/holden/file_io/file_io.py
+++ dev/holden/file_io/file_io.py
@@ -2,8 +2,12 @@
 import subprocess
 
 '''
-lsusb - shell command to list all usb devices connected
+lsusb -> shell command to list all usb devices connected
   may be useful for finding USB & identifying webcams
+
+usb-devices -> Another shell command. Seems to be a more
+  verbose version than lsusb. May have more connections
+  displayed as well.
 '''
 
 def mount_drive(): subprocess.Popen('sudo mount /dev/sda1 /mnt/usb -o uid=pi,gid=pi')
