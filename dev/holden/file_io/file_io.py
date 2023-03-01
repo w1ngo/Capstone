df32d8fdc4a60e1bfa3b0ccc016eb0c4d4751832
w1ngo <65159308+w1ngo@users.noreply.github.com>
2023-03-01 09:52:48 -0600

    Update file_io.py

--- dev/holden/file_io/file_io.py
+++ dev/holden/file_io/file_io.py
@@ -1,6 +1,11 @@
 
 import subprocess
 
+'''
+lsusb - shell command to list all usb devices connected
+  may be useful for finding USB & identifying webcams
+'''
+
 def mount_drive(): subprocess.Popen('sudo mount /dev/sda1 /mnt/usb -o uid=pi,gid=pi')
 def unmount_drive(): subprocess.Popen('sudo unmount /mnt/usb')
 
