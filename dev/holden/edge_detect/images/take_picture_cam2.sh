366f0efe92f5d62080ff790b914b9e0a0023e08c
Holden Hill <holden.onestarflag@gmail.com>
2023-03-02 14:21:25 -0600

    Add scripts for camera control & dependency to init
    Two files were added, one for each camera.
    There is a lot of customization that can be done,
    but initial testing at least can be done with them as-is.
    
    Each script is essentially a one-liner with comments, so they
    can be converted to python using the subprocess module present
    in other areas already (such as file i/o)

---
+++ dev/holden/edge_detect/images/take_picture_cam2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+fswebcam --device /dev/video2  \
+	 --quiet               \
+	 --resolution 640x480  \
+	 -s sharpness=15       \
+	 --frame 10            \
+	 --no-timestamp        \
+	 --no-banner           \
+	 --no-info             \
+	 --greyscale           \
+	 --png 0               \
+	 --save                \
+         image.png
+
+# line below prints controls that can be applied to the camera
+# fswebcam -d /dev/video0 --list-controls
+ 
+
+
